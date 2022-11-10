fbc2076c8daac5fb8d0b0fbc4343071379756876
seungchae <s2125@e-mirim.hs.kr>
2022-11-10 18:37:54 +0900

    login box

--- main/main.js
+++ main/main.js
@@ -1,1 +1,12 @@
-
+//로그인 박스
+let d=document.getElementsByClassName("dialog")[0];
+function dia(){
+    d.style.width="500px"
+    d.style.height="300px";
+    d.style.visibility="visible";
+}
+function close_d(){
+    d.style.visibility="hidden";
+    d.style.width="0px"
+    d.style.height="0px";
+}
