1dc8409075fdc6cc486f95723ec56be7d8587c1e
seungchae <s2125@e-mirim.hs.kr>
2022-11-11 21:59:22 +0900

    로그인/회원가입 수정

--- join.php
+++ join.php
@@ -10,9 +10,9 @@ $uaddress=$_POST['uaddress'];
 $sql = "select id from hobby_join;";
 $sel = mysqli_query($conn, $sql);
 $n = mysqli_num_rows($sel); 
-for($i=0; $i<$n; $i++){ $re=mysqli_fetch_row($sel); if((string)$re[0]==$uid){echo "<script>alert('이미 사용중인 아이디입니다.'); history.go(-1);</script>";}}
 if($uid == null) echo "<script>alert('아이디를 입력하세요.'); history.go(-1);</script>"; 
-else if($upass == null) echo "<script>alert('비밀번호를 입력하세요.'); history.go(-1);</script>"; 
+else for($i=0; $i<$n; $i++){ $re=mysqli_fetch_row($sel); if((string)$re[0]==$uid){echo "<script>alert('이미 사용중인 아이디입니다.'); history.go(-1);</script>"; break;}}
+if($upass == null) echo "<script>alert('비밀번호를 입력하세요.'); history.go(-1);</script>"; 
 else if($ch_pass == null) echo "<script>alert('비밀번호를 확인하세요.'); history.go(-1);</script>"; 
 else if($uname == null) echo "<script>alert('이름을 확인하세요.'); history.go(-1);</script>"; 
 else if($upass != $ch_pass ) echo "<script>alert('비밀번호 확인이 일치하지 않습니다.'); history.go(-1);</script>"; 
--- login.php
+++ login.php
@@ -7,8 +7,10 @@ $sql = "select id, pass from hobby_join where id='$uid' && pass='$upass';";
 $sel = mysqli_query($conn, $sql);
 $n = mysqli_num_rows($sel);
 
-if($n==0) echo "<script>alert('아이디 또는 비밀번호가 틀렸습니다.');  history.go(-1);</script>";
+if($n==0) echo "<script>alert('아이디 또는 비밀번호가 틀렸습니다.'); history.go(-1);</script>";
 else{
-    echo "환영합니다.";
+    setcookie("uid",$uid,time()+3600);
+    setcookie("upass",$upass,time()+3600);
+    echo "<meta http-equiv='refresh' content='2;url=index.html'>";
 }
 ?>
