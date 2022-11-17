2e89dc7448a2aad83438fe21e675e6a3a700658e
seungchae <s2125@e-mirim.hs.kr>
2022-11-17 09:45:00 +0900

    댓글 구현

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
               $row= mysqli_fetch_array($result);
             ?>
 
-            <tr onClick="location.href='user_write.php?id=<?php $row[0] ?>'" class="list_tr">
+            <tr onClick="location.href='user_write.php?id=<?php echo $row[0]; ?>'" class="list_tr">
 
 	            <td class="list_td"><?php echo $row[0];?></td>
 	            <td class="list_td"><?php echo $row[1]; ?></td>
@@ -168,7 +168,7 @@
               $row= mysqli_fetch_array($result);
             ?>
 
-            <tr onClick="location.href='user_write.php?id=<?php $row[0] ?>'" class="list_tr"><!-- 첫번째 줄 시작 -->
+            <tr onClick="location.href='user_write.php?id=<?php echo $row[0]; ?>'" class="list_tr"><!-- 첫번째 줄 시작 -->
 
 	            <td class="list_td"><?php echo $row[0];?></td>
 	            <td class="list_td"><?php echo $row[1]; ?></td>
---
+++ write_comm.php
@@ -0,0 +1,10 @@
+<?php
+ include("./db_conn.php");
+ if(!isset($_COOKIE['uid'])) echo "<script>alert('로그인이 필요합니다.'); history.go(-1);</script>";
+ else $id = $_COOKIE['uid'];
+ $num = $_POST['num'];
+ $content = $_POST['comment'];
+ $sql = "insert into comm value($num, '$content', '$id');";
+ mysqli_query($conn, $sql);
+ echo "<meta http-equiv='refresh' content='0;url=./user_write.php?id=$num'>";
+?>
