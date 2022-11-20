f219e15c2c0083acc19955d58b72b657350b88ac
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-11-20 23:38:25 +0900

    전체게시판 수정

--- index.php
+++ index.php
@@ -46,30 +46,21 @@
                 <div id="subwrapper">
                   <nav id="subnav">
                     <ul id="submenu__list">
-<<<<<<< HEAD
 
-=======
->>>>>>> 71501acaba09fa32178e89717d000c779909f7d0
                       <li id="submenu__menu">공예/만들기</li>
                       <li id="submenu__menu">그림</li>
                       <li id="submenu__menu">노래/작사/작곡</li>
                       <li id="submenu__menu">악기</li>
                       <li id="submenu__menu">사진</li>
                       <li id="submenu__menu">패션</li>
-<<<<<<< HEAD
 
-=======
->>>>>>> 71501acaba09fa32178e89717d000c779909f7d0
                       <li id="submenu__menu"><a href="./ctgr1.php">공예/만들기</a></li>
                       <li id="submenu__menu"><a href="./ctgr2.php">그림</a></li>
                       <li id="submenu__menu"><a href="./ctgr3.php">노래/작사/작곡</a></li>
                       <li id="submenu__menu"><a href="./ctgr4.php">악기</a></li>
                       <li id="submenu__menu"><a href="./ctgr5.php">사진</a></li>
                       <li id="submenu__menu"><a href="./ctgr6.php">패션</a></li>
-<<<<<<< HEAD
 
-=======
->>>>>>> 71501acaba09fa32178e89717d000c779909f7d0
                     </ul>
                   </nav>
                 </div>
@@ -131,7 +122,7 @@
 
         <div class="notice_rule"> <!-- 공지사항/규칙 -->
           <h4 id="rule">공지사항/ 규칙</h4>
-<<<<<<< HEAD
+
 
           
 
@@ -144,14 +135,9 @@
             <th id="list1_name">조회</th>
             
 	          
-=======
-          <table border="1" class="list">
-	          <th>번호</th>
-	          <th>글쓴이</th>
-            <th>제목</th>
-            <th>등록일</th>
-            <th>조회</th>
->>>>>>> 71501acaba09fa32178e89717d000c779909f7d0
+
+          
+
           <?php
             include("db.php");
             $query ="select * from hobbycountry_write where id=1";
@@ -180,18 +166,20 @@
           
           <!-- 리스트 -->
           <table class="list_2">
-            <div id="list_name">
+            
+          <div id="list_name">
               <th id="list2_name">번호</th>
               <th id="list2_name">제목</th>
               <th id="list2_name">글쓴이</th>
               <th id="list2_name">등록일</th>
               <th id="list2_name">조회</th>
             </div>
+           
 
             <?php 
             include('./db.php');
 
-            $query ="select * from write_h order by id desc";
+            $query ="select * from hobbycountry_write order by id desc";
             $result=mysqli_query($conn, $query);
 
             $count=mysqli_num_rows($result);
@@ -200,25 +188,14 @@
               $row= mysqli_fetch_array($result);
             ?>
 
-<<<<<<< HEAD
-
-            <tr onClick="location.href='user_write.php?id=<?php echo $row[0] ?>'"><!-- 첫번째 줄 시작 -->
-
-	            <td class="list_td2"><?php echo $row[0];?></td>
-              <td class="list_title2"><?php echo $row[1]; ?></td>
-              <td class="list_td2"><?php echo $row[5]; ?></td>
-              <td class="list_td2"><?php echo $row[6]; ?></td>
-              <td class="list_td2"><?php  ?></td>
-
-            
             
             <tr onClick="location.href='user_write.php?id=<?php echo $row[0]; ?>'" class="list_tr"><!-- 첫번째 줄 시작 -->
 
-	            <td class="list_td"><?php echo $row[0];?></td>
-	            <td class="list_td"><?php echo $row[1]; ?></td>
-              <td class="list_td"><?php echo $row[2]; ?></td>
-              <td class="list_td"><?php echo $row[5]; ?></td>
-              <td class="list_td"><?php echo $row[7]; ?></td>
+	            <td class="list_td2"><?php echo $row[0];?></td>
+	            <td class="list_title2"><?php echo $row[1]; ?></td>
+              <td class="list_td2"><?php echo $row[2]; ?></td>
+              <td class="list_td2"><?php echo $row[5]; ?></td>
+              <td class="list_td2"><?php echo $row[7]; ?></td>
 	          </tr><!-- 첫번째 줄 끝 -->
 
             <?php }
@@ -226,25 +203,18 @@
           </table>
 
           <!-- 리스트 -->
-<<<<<<< HEAD
-
-=======
->>>>>>> 71501acaba09fa32178e89717d000c779909f7d0
 
           <span style='float:right'>
             <button type="button" id="write" class="btn btn-default" ><a href="write.html">글쓰기</a></button>
           </span>
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 71501acaba09fa32178e89717d000c779909f7d0
         </div>
         <div id="footer">
           <h5>문의사항</h5>
           s2125@e-mirim.hs.kr</br>
           s2117@e-mirim.hs.kr
-          <h4 id="footer_name" src="./img/">취미나라</h4>
+          <h4 id="footer_name" src="img/logo.png/">취미나라</h4>
         </div>
 
 
